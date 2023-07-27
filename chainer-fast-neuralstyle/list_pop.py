6ff6586e82f6b34f4a95df9ee2c3e05bb36b52ab
Niccolò Perego <niccolo.perego@mail.polimi.it>
2023-07-27 17:00:41 +0200

    update list_pop.py

--- chainer-fast-neuralstyle/list_pop.py
+++ chainer-fast-neuralstyle/list_pop.py
@@ -1,14 +1,29 @@
 import os
+import argparse
 
-dataset = './coco/train2014'
+parser = argparse.ArgumentParser(description='Real-time style transfer')
+parser.add_argument('--force_update', '-f', default=False,
+                    action=argparse.BooleanOptionalAction)
+parser.add_argument('--dataset', '-d', default='./coco/train2014', type=str)
+args = parser.parse_args()
 
-if os.path.exists(f'{dataset}/../fs.list'):
+dataset = args.dataset
+force_update = args.force_update
+fs_path = f'{dataset}/../fs.list'
+
+fs_exixts = os.path.exists(fs_path)
+
+if fs_exixts and not force_update:
     # read from file with names to save time
     print("reading fs.list")
-    with open(f'{dataset}/../fs.list') as f:
+    with open(fs_path, 'r') as f:
         imagepaths = f.read().splitlines()
 else:
     # one off, create file with image names
+    if force_update and fs_exixts:
+        print("deleting fs.list")
+        os.remove(fs_path)
+
     print("reading dataset directory")
     fs = os.listdir(dataset)
     imagepaths = []
@@ -18,7 +33,7 @@ else:
             imagepath = os.path.join(dataset, fn)
             imagepaths.append(imagepath)
     print("saving in fs.list") 
-    with open(f'{dataset}/../fs.list', 'w') as tfile:
+    with open(fs_path, 'w') as tfile:
         tfile.write('\n'.join(imagepaths))
 
 n_data = len(imagepaths)
